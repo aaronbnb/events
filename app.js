2ad6dfe88b2ea41c5d4f9260591d0102a9be2a02
Aaron Farber <aaronfarber9@gmail.com>
2020-08-23 13:16:48 -0700

    first

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 document.getElementById('test1').addEventListener('click', (e) => {
-    e.target.style.color = "black" ? "red" : "black";
+
 })
 
 document.getElementById('test2').addEventListener('keypress', function(e) {
-    e.target.style.color = "black" ? "red" : "black";
+    if (e.target.style.color === "black") {
+        e.target.style.color = "red";
+    } else {
+        e.target.style.color = "black"
+    }
 })
+
+document.getElementById('test2').addEventListener('click', function(e) {
+    if (e.target.style.color === "black") {
+        e.target.style.color = "red";
+    } else {
+        e.target.style.color = "black"
+    }
+})
+
 
 document.querySelectorAll('sas').forEach(item => {
     item.addEventListener('click', event => {
