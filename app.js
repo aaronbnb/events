8302122c6c344beefff8c193f99129fbbb0dc913
Aaron Farber <aaronfarber9@gmail.com>
2020-08-23 11:03:11 -0700

    first

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 document.getElementById('test1').addEventListener('click', (e) => {
-    e.style.color = "black" ? "red" : "black";
+    e.target.style.color = "black" ? "red" : "black";
 })
 
 document.getElementById('test2').addEventListener('keypress', function() {
-    e.style.color = "black" ? "red" : "black";
+    e.target.style.color = "black" ? "red" : "black";
 })
 
 document.querySelectorAll('sas').forEach(item => {
